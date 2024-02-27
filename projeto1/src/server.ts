2c8657dba5434a2fc3d80fe99c8db5a7b1243ec8
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-02-27 01:52:45 -0300

    Aula 4

---
+++ projeto1/src/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get("/",()=>{
+  return "OI"
+})
+
+app.listen({
+  port:3333
+}).then(()=>{
+  console.log("Deu muito certo");
+  
+})
